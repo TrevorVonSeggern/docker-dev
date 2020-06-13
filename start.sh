61af547d2663dcb758fef9fb7bd0db43fded4738
Trevor Von Seggern <tvoman2@gmail.com>
2020-06-12 20:57:48 -0600

    init start script

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [[ "$(docker images -q devbox 2> /dev/null)" == "" ]]; then
-	docker build -t devbox .
+echo "Do you wish to install this program?"
+select RUN in "dotnet" "python" ;do break; done;
+echo setting up environment for $RUN
+
+if [[ "$(docker images -q devbox.$RUN 2> /dev/null)" == "" ]]; then
+	docker build -f Dockerfile.$RUN -t devbox.$RUN .
 fi
 
 docker run -it \
-	-v `pwd`/dotfiles:/home/devuser \
-	-v ~/.ssh:/home/devuser/.ssh \
-	devbox
+	-v `pwd`/dotfiles:/home/user \
+	-v ~/.ssh:/home/user/.ssh \
+	devbox.$RUN
 
