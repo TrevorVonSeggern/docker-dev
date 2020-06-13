1418a0bd875e96870d132dcdd7831fd54dcc4d5f
Trevor Von Seggern <tvoman2@gmail.com>
2020-06-12 22:31:24 -0600

    got dotnet working at a basic level

--- start.sh
+++ start.sh
@@ -4,12 +4,13 @@ echo "Do you wish to install this program?"
 select RUN in "dotnet" "python" ;do break; done;
 echo setting up environment for $RUN
 
-if [[ "$(docker images -q devbox.$RUN 2> /dev/null)" == "" ]]; then
+#if [[ "$(docker images -q devbox.$RUN 2> /dev/null)" == "" ]]; then
 	docker build -f Dockerfile.$RUN -t devbox.$RUN .
-fi
+#fi
 
+# might want to add this current mappinf or an alias
+	#-v `pwd`/dotfiles:/home/user/work \
 docker run -it \
-	-v `pwd`/dotfiles:/home/user \
 	-v ~/.ssh:/home/user/.ssh \
 	devbox.$RUN
 
